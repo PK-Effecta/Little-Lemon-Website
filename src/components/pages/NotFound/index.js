536f7a38a19c474e1a7a1b78dd1a505460674acc
PK-Effecta <141636380+PK-Effecta@users.noreply.github.com>
2023-09-25 18:59:04 +0200

    Add files via upload

---
+++ src/components/pages/NotFound/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+const NotFound = () => {
+  return (
+    <div className="container page-not-found">
+      <h2>This page is not found. Go back and discover our delicious food</h2>
+    </div>
+  );
+};
+
+export default NotFound;
